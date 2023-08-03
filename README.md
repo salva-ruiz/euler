00d43e8d63b2a61181523fc7b885b98dd9b0209f
Salva Ruiz <cuentas@salvaruiz.me>
2023-08-04 01:37:18 +0200

    Created the project and the Mix task.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Euler
+
+Solution to some of the challenges of the [Project Euler](https://projecteuler.net/about).
+
+## Test
+
+Run the `mix test` command.
+
+## Solutions
+
+To print the solutions of challenges, run the `mix euler` task.
---
+++ lib/mix/tasks/euler.ex
@@ -0,0 +1,31 @@
+defmodule Mix.Tasks.Euler do
+  @moduledoc """
+  Solution to some of the challenges of [Project Euler](https://projecteuler.net/about).
+  """
+
+  use Mix.Task
+
+  @shortdoc "Print the challenges of the Euler project"
+
+  @solutions [
+  ]
+
+  @impl Mix.Task
+  def run(_args) do
+    @solutions
+    |> Stream.with_index(1)
+    |> Stream.each(fn {{module, fun, params}, number} -> print(number, module, fun, params) end)
+    |> Stream.run()
+  end
+
+  defp print(number, module, function, params) do
+    IO.puts("""
+    Exercise #{number}
+    Name     #{function |> Atom.to_string() |> String.replace("_", " ")}
+    Module   #{module |> Atom.to_string() |> String.replace_leading("Elixir.", "")}
+    Function #{function |> Atom.to_string()}
+    Params   #{params |> inspect()}
+    Result   #{apply(module, function, params) |> inspect()}
+    """)
+  end
+end
